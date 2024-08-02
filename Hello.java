5af505ffc922610c0f70dd241122b0c5484d8d6f
fayoola <fayola_luvena_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:01:15 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+public class Hello {
+    public static void main(String [] args) {
+
+        String nama= "Fayola";
+        String asalKota= "Mojokerto";
+        int usia=15;
+        double beratBadan=45.00;
+        float tinggiBadan=150.0f;
+        char jenisKelamin = 'P';
+        boolean status = false;
+
+        System.out.println("Nama: "+nama);
+        System.out.println("Asal Kota: "+asalKota);
+        System.out.println("Usia: "+usia);
+        System.out.println("Berat Badan: "+beratBadan);
+        System.out.println("Tinggi Badan: "+tinggiBadan);
+        System.out.println("Jenis Kelamin: "+jenisKelamin);
+        System.out.println("Status: "+status);
+
+    }  
+}
